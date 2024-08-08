bc16091151f51e701f2b5c56ec99a7c71c79f436
gregory223c <gregory223chavez@outlook.com>
2024-08-08 14:08:55 +0530

    Create Receipt.js

---
+++ public/src/components/Receipt.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+    return (
+        <div className="receipt">
+            <h2>Your Spending Summary</h2>
+            <p>Here will be the detailed receipt of your expenses.</p>
+        </div>
+    );
+}
+
+export default Receipt;
